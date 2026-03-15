c678719fa4b2d72e481e40b06b4c1f6ec90b8407
dew-ls <artemsuprankov6388@gmail.com>
2026-03-15 18:05:57 +0300

    Fifth version of code.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main()
+int
+main()
 {
-	char * name; // переменная для имени пользователя
-	std::cout << "Enter your name: " ; std::cin >> name; // инициализация переменной
-	std::cout << "Hello world from " << name << endl; // вывод
-	return 0;
+  char* name; // varible for user's name
+  std::cout << "Enter your name: ";
+  std::cin >> name;                                 // initialising name
+  std::cout << "Hello world from " << name << endl; // output
+  return 0;
 }
